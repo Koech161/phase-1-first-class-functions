2336970d520a44ebed0fefa26626754c81a4720c
Abel Koech <koechabel161@gmail.com>
2024-06-26 20:24:43 +0300

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback){
+    callback();
+}
+function returnsANamedFunction(){
+    let fn;
+    return function fn (){
+    
+    }
+}
+function returnsAnAnonymousFunction(){
+    return function  (){
+
+    }
+}
